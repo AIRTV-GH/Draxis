d51e666184ff0b883a6e5537c361826986784421
Ismail Ozsaygi <devozsaygi@gmail.com>
2018-01-10 17:36:47 +0300

    ewo Commit (1)
      - Overview -
    * Added intro scene to place a splash art.
    * Added "_common" & "_utils" folders to "_src" folder.
    * Added "Display" class to "_common" folder.
    * Added "Initialization" class to "_common" folder.

---
+++ Draxis/Assets/_src/_common/Display.cs
@@ -0,0 +1,59 @@
+﻿
+//*********************************************************
+//
+// $author  : Ismail Ozsaygi
+// $purpose : Handling the multiple screen resolutions & setting resolution manually.
+// $attach  : "Initialization.cs" will auto add this to "Application Manager" game object at "scene_intro"
+//
+//*********************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Keep window properties as one.
+struct wnd_properties
+{
+    public int  wnd_width;
+    public int  wnd_height;
+    public bool wnd_isFullScreen;
+}
+
+class Display : MonoBehaviour 
+{
+
+    private wnd_properties new_window;
+
+    //**********
+    // Get the native resolution from system & apply it. (Auto resolution detection)
+    //**********
+	public void display_set_res()
+    {
+        // Get resolution here.
+        new_window.wnd_width        = Screen.currentResolution.width;
+        new_window.wnd_height       = Screen.currentResolution.height;
+        new_window.wnd_isFullScreen = true;
+
+        // Apply window properties.
+        Screen.SetResolution( new_window.wnd_width , new_window.wnd_height , new_window.wnd_isFullScreen );
+
+        // For testing (can be removed later)
+        Debug.Log( "Width       : " + new_window.wnd_width.ToString() );
+        Debug.Log( "Height      : " + new_window.wnd_height.ToString() );
+        Debug.Log( "Full Screen : " + new_window.wnd_isFullScreen.ToString() );
+    }
+
+    //**********
+    // Just a overload function for resolution.
+    // With this function we can set the resolution manually when we need it.
+    //**********
+    public void display_set_res( int p_width , int p_height , bool p_isFullScreen )
+    {
+        new_window.wnd_width        = p_width;
+        new_window.wnd_height       = p_height;
+        new_window.wnd_isFullScreen = p_isFullScreen;
+
+        Screen.SetResolution( new_window.wnd_width , new_window.wnd_height , new_window.wnd_isFullScreen );
+    }
+
+}
---
+++ Draxis/Assets/_src/_common/Initialization.cs
@@ -0,0 +1,26 @@
+﻿
+//*********************************************************
+//
+// $author  : Ismail Ozsaygi
+// $purpose : Loads the application settings at "scene_intro"
+// $attach  : Will be attached to "Application Manager" game object at "scene_intro"
+//
+//*********************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ RequireComponent( typeof( Display ) ) ]
+public class Initialization : MonoBehaviour 
+{
+
+	private Display new_display;
+
+    private void Start()
+    {
+        new_display = GetComponent< Display >();
+        new_display.display_set_res();
+    }
+
+}
