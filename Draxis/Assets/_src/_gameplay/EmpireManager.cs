ed60dd79e109e13b90529bd24af490345a5c7f88
Cstough <32025644+Cstough@users.noreply.github.com>
2018-01-14 12:56:50 -0500

    More Materials

--- Draxis/Assets/_src/_gameplay/EmpireManager.cs
+++ Draxis/Assets/_src/_gameplay/EmpireManager.cs
@@ -16,21 +16,21 @@ public class EmpireManager : MonoBehaviour {
     [SerializeField]
     public List<City> cities; //List of cities in our empire. Can be used to prevent overlapping cities, min distance between, displaying a list of all cities, etc.
     public GameObject city;
-    public float cityDensity;
+    public float citySpacing;
 
     public void Start()
     {
         cities = new List<City>();
     }
 
-    public bool cityDensityCheck(Vector2 cityPos)
+    public bool cityCheck(Vector2 cityPos)
     {
         bool pass = false;
         if (cities.Count > 0)
         {
             foreach (City c in cities)
             {
-                if (Vector2.Distance(cityPos, c.cityPosition) < cityDensity)
+                if (Vector2.Distance(cityPos, c.cityPosition) < citySpacing)
                 {
                     pass = false;
                     break;
@@ -51,13 +51,11 @@ public class EmpireManager : MonoBehaviour {
 
     public void addCity(Vector2 cityPos, string cityName) //gets called when a city is created
     {
-        if (cityDensityCheck(cityPos))
+        if (cityCheck(cityPos))
         {
             cities.Add(new City(cityName, cityPos)); //Adds a new City object to the List: cities.
             GameObject newCity = Instantiate(city, new Vector3(cityPos.x, 0.75f, cityPos.y), Quaternion.identity); //Instantiates the city model
             newCity.transform.parent = gameObject.transform;
-            //Used for testing purposes
-            Debug.Log("City Name: " + cityName + " City Position: " + cityPos);
         }
     }
 }
