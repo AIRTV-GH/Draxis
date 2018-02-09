761171dc8aa92ff7dccb2724b2680869baba5aef
Ismail Ozsaygi <devozsaygi@gmail.com>
2018-02-09 11:36:15 +0200

    Fps Tool Update
    * Added simple fps tool to display the ingame fps.So we can see the performance state ingame.We can toggle the fps with a button that placed in UI.

---
+++ Draxis/Assets/_src/_ui/FpsButton.cs
@@ -0,0 +1,22 @@
+﻿
+//*********************************************************
+//
+// $author  : Ismail Ozsaygi
+// $purpose : Toggles the fps display state.
+// $attach  : Attach to Fps button under user interface.
+//
+//*********************************************************
+
+using UnityEngine;
+
+public class FpsButton : MonoBehaviour 
+{
+    // Call from button's click event.
+    public void ToggleState()
+    {
+        if( Fps.IsEnabled )
+            Fps.IsEnabled = false;
+        else
+            Fps.IsEnabled = true;
+    }
+}
---
+++ Draxis/Assets/_src/_utils/Fps.cs
@@ -0,0 +1,55 @@
+﻿
+//*********************************************************
+//
+// $author  : Ismail Ozsaygi
+// $purpose : Displays the current fps so we can see the performance.
+// $attach  : Attach it to FPS UI text object.Under user interface.
+//
+//*********************************************************
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fps : MonoBehaviour 
+{
+    [ SerializeField ]
+    public static bool   IsEnabled = false;
+
+    private Text  FpsTexT;
+
+    // FPS definitions.
+    private float        FrameCount  = 0.0f;
+    private float        DiffRate    = 0.0f;
+    private float        CurrentFps  = 0.0f;
+    private float        UpdateRate  = 4.0f;
+
+    private void Start()
+    {
+        FpsTexT    = GetComponent< Text >();
+    }
+
+    private void Update()
+    {
+        CalculateFps();
+    }
+
+    private void CalculateFps()
+    {
+        if( IsEnabled )
+        {
+            FrameCount++;
+            DiffRate        += Time.deltaTime;
+            if( DiffRate > 1.0f / UpdateRate )
+            {
+                CurrentFps   = FrameCount / DiffRate;
+                FrameCount   = 0;
+                FpsTexT.text = "FPS : " + CurrentFps.ToString();
+                DiffRate    -= 1.0f / UpdateRate;
+            }
+        }
+        else
+        {
+            FpsTexT.text     = "";
+        }
+    }
+}
